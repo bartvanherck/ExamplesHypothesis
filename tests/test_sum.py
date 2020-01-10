449dd18361a04ee5f7dd72d721d75a9955ae9ae9
Bart Vanherck <vherckbart@gmail.com>
2020-01-10 21:34:34 +0100

    still buggy code

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,4 +1,7 @@
 import pytest
+from hypothesis import given
+import hypothesis.strategies as st
+
 
 def sum(num1, num2):
     if num1 == 3 and num2 == 5:
@@ -11,7 +14,13 @@ def sum(num1, num2):
         return -2
     elif num1 == 0 and num2 == 5:
         return 5
+    return num1 + num2
 
 @pytest.mark.parametrize('num1, num2, expected',[(3,5,8),(-2,-2,-4),(-1,5,4),(3,-5,-2),(0,5,5)])
 def test_sum(num1, num2, expected):
     assert sum(num1, num2) == expected
+
+
+@given(st.integers(), st.integers())
+def test_sum_1(num1, num2):
+    assert sum(num1, num2) == num1 + num2
