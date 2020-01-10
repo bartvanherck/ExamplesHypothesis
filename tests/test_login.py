a61cab8acfcf85eb46f05a63336958355c148d1e
Bart Vanherck <vherckbart@gmail.com>
2020-01-10 21:16:37 +0100

    Update the tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,9 @@
 from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis.strategies import from_regex
 from login import Login 
 
 
-@given(text(min_size=8, max_size=12))
+@given(from_regex("^[a-zA-Z0-9_]{8,12}$",fullmatch=True))
 def test_check_password(pwd):
     login = Login()
     login.password  = pwd
